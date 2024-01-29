b4304a0fc79701695896a2ec6750cf0ef3d364ae
piaan <VianRiztanta71@gmail.com>
2024-01-29 22:51:20 +0700

    Update 2024_01_27_160832_create_student_school_fees_table.php

--- database/migrations/2024_01_27_160832_create_student_school_fees_table.php
+++ database/migrations/2024_01_27_160832_create_student_school_fees_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('student_school_fees', function (Blueprint $table) {
             $table->id();
             $table->foreignId('student_id')->constrained('students');
-            $table->foreignId('school_fees_id')->constrained('school_fees');
+            $table->foreignId('school_fee_id')->constrained('school_fees');
             $table->date('date_of_payment');
             $table->enum('month', ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'])->nullable();
             $table->timestamps();
